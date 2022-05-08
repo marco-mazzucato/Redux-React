74cbef6d37ca2e537690dbeafe515d5425a1d71f
marco-mazzucato <marco.mazzu99@gmail.com>
2022-05-08 19:04:56 +0200

    tolti commenti

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState } from 'react';  
+
+const navbar = (
+    <ul>
+    <li><a className="active" href="#home">Home</a></li>
+    <li><a href="#news">News</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#about">About</a></li>
+  </ul>  
+  )
+
+function MainContent() {
+  return (
+    <h1>This is the main content</h1>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button className= "agg" onClick={() => setCount(count+1)}>aggiungi</button>
-        <button className= "agg" onClick={() => setCount(count-1)}>togli</button>
-        <p>{count}</p>
-      </header>
+      {navbar}
+      <button className='agg' onClick={() => setCount(count +1)}>Aumenta</button>
+      <button className='agg' onClick={() => setCount(count -1)}>Diminuisci</button>
+      <h1>{count}</h1>
+      <MainContent />
     </div>
   );
 }
